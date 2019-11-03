bd974ad76bb63791cf38ae6dc593c434a5c862fe
Kaspar Bach Pedersen <kasparp@me.com>
2019-11-03 18:01:52 +0100

    start unit testing api client

---
+++ clouddk/api_test/clouddksvc_test.go
@@ -0,0 +1,54 @@
+package api_test
+
+import (
+	"context"
+	"github.com/KasparBP/cloud-dk-provider/clouddk/api"
+	"gotest.tools/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"testing"
+)
+
+func loadTestData(t *testing.T, fname string) []byte {
+	path := filepath.Join("testdata", fname)
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return bytes
+}
+
+// Setup test server which will respond with testdata loaded from supplied filename
+func setupTestServer(t *testing.T, fname string) *httptest.Server {
+	response := loadTestData(t, fname)
+	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// Send response to be tested
+		_, err := rw.Write(response)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}))
+}
+
+func testClient(t *testing.T, s *httptest.Server) *api.Client  {
+	client, err := api.NewClient("token", s.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return client
+}
+
+func TestGetCloudServer(t *testing.T) {
+	server := setupTestServer(t, "getcloudserver_resp.json")
+	defer server.Close()
+
+	client := testClient(t, server)
+	response, err := client.ClouddkService.GetCloudServer(context.Background(), "identifier1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Assert(t, response.Booted)
+	// TODO
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/KasparBP/cloud-dk-provider
 
 go 1.13
 
-require github.com/hashicorp/terraform-plugin-sdk v1.2.0
+require (
+	github.com/hashicorp/terraform-plugin-sdk v1.2.0
+	github.com/pkg/errors v0.8.1 // indirect
+	gotest.tools v2.2.0+incompatible
+)
